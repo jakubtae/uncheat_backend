4ae0f54b00c2eeb677761c572abd501323377fe3
Jakub <jakub.tomczyk.2005@gmail.com>
2024-03-02 10:32:08 +0100

    get router

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,8 @@ import express from "express";
 import RandomTestInf02 from "#root/controllers/findRand.js";
 
 const router = express.Router();
-
+router.get("/", (req, res) => {
+  res.json("Hello server");
+});
 router.get("/:type/:number", RandomTestInf02);
 export default router;
